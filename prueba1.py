378db60c1ec8c5afa381e12238e4d05fbf83e09a
axelsot0 <axelmlsoto@gmail.com>
2022-12-07 11:27:52 -0400

    prueba de tkinter ian

---
+++ prueba1.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+#creacion de ventana
+ventana = Tk()
+
+ventana.configure(width= "400", height="400", bg="black")
+ventana.title("Programa de prueba")
+
+def hola():
+    print("Hola Ian")
+
+boton1 = Button(text="saludar", fg="black", bg = "white", command= lambda: hola())
+
+boton1.grid(row= 5, column= 5, padx = 5, pady= 5)
+
+
+ventana.mainloop()
